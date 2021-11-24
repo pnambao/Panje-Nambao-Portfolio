913c98fff96bad08ff3708c4d5bf2b9abd100712
RoachCoach <panjenambao@gmail.com>
2021-11-23 19:11:37 -0600

    added components folder and assets folder

---
+++ panje-portfolio/src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Home.css';
+function Home() {
+    return (
+        <div className = "home">
+           <div className = "home__bg">
+
+           </div>
+        </div>
+    )
+}
+
+export default Home;
